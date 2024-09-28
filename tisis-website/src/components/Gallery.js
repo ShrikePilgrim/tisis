e864074adc3a2d1753a9012e909ffb454c2dff86
ShrikePilgrim <dimitris.risis@outlook.com>
2024-09-28 16:00:54 +0300

    Added thumbnails to the videos in the video carousel to enhance performance, also added svgs for the play and open buttons on the carousels.

--- tisis-website/src/components/Gallery.js
+++ tisis-website/src/components/Gallery.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Slider from "react-slick";
 import LazyLoad from 'react-lazyload';
 import "slick-carousel/slick/slick.css";
@@ -23,6 +23,23 @@ import bunkerVideo from "../assets/videos/bunker.mp4";
 import nightfallVideo from "../assets/videos/nightfall.mp4";
 import village3Video from "../assets/videos/village3.mp4";
 
+import asylum1Thumbnail from "../assets/images/thumbnails/asylum1Thumbnail.png";
+import asylum2Thumbnail from "../assets/images/thumbnails/asylum2Thumbnail.png";
+import asylum3Thumbnail from "../assets/images/thumbnails/asylum3Thumbnail.png";
+import asylum4Thumbnail from "../assets/images/thumbnails/asylum4Thumbnail.png";
+import bunkerThumbnail from "../assets/images/thumbnails/bunkerThumbnail.png";
+import expeditionThumbnail from "../assets/images/thumbnails/expeditionThumbnail.png";
+import forest1Thumbnail from "../assets/images/thumbnails/forest1Thumbnail.png";
+import forest2Thumbnail from "../assets/images/thumbnails/forest2Thumbnail.png";
+import hyadesThumbnail from "../assets/images/thumbnails/hyadesThumbnail.png";
+import lake1Thumbnail from "../assets/images/thumbnails/lake1Thumbnail.png";
+import lake2Thumbnail from "../assets/images/thumbnails/lake2Thumbnail.png";
+import nightfallThumbnail from "../assets/images/thumbnails/nightfallThumbnail.png";
+import templeThumbnail from "../assets/images/thumbnails/templeThumbnail.png";
+import village1Thumbnail from "../assets/images/thumbnails/village1Thumbnail.png";
+import village2Thumbnail from "../assets/images/thumbnails/village2Thumbnail.png";
+import village3Thumbnail from "../assets/images/thumbnails/village3Thumbnail.png";
+
 import antarcticImage from "../assets/images/gallery/expedition8.png";
 import temple2Image from "../assets/images/gallery/temple2.png";
 import bunker1Image from "../assets/images/gallery/bunker1.png";
@@ -43,6 +60,9 @@ import village12Image from "../assets/images/gallery/village12.png";
 function Gallery() {
   const [isFullscreen, setIsFullscreen] = useState(false);
   const [fullscreenImage, setFullscreenImage] = useState(null);
+
+  const [loadedVideos, setLoadedVideos] = useState([]);
+  const videoRefs = useRef({});
 
   const handleImageClick = (image) => {
     setFullscreenImage(image);
@@ -66,16 +86,25 @@ function Gallery() {
     event.target.src = placeholderImage;
   };
 
-  const handleVideoClick = (videoElement) => {
-    if (videoElement.requestFullscreen) {
-      videoElement.requestFullscreen();
-    } else if (videoElement.webkitRequestFullscreen) { // Safari support
-      videoElement.webkitRequestFullscreen();
-    } else if (videoElement.mozRequestFullScreen) { // Firefox support
-      videoElement.mozRequestFullScreen();
-    } else if (videoElement.msRequestFullscreen) { // IE/Edge support
-      videoElement.msRequestFullscreen();
-    }
+  const handleVideoClick = (index) => {
+    setLoadedVideos(prev => [...prev, index]);
+    setTimeout(() => {
+      const videoElement = videoRefs.current[index];
+
+      if (videoElement) {
+        videoElement.play();
+
+        if (videoElement.requestFullscreen) {
+          videoElement.requestFullscreen();
+        } else if (videoElement.webkitRequestFullscreen) { // Safari support
+          videoElement.webkitRequestFullscreen();
+        } else if (videoElement.mozRequestFullScreen) { // Firefox support
+          videoElement.mozRequestFullScreen();
+        } else if (videoElement.msRequestFullscreen) { // IE/Edge support
+          videoElement.msRequestFullscreen();
+        }
+      }
+    }, 100);
   };
 
   useEffect(() => {
@@ -136,81 +165,97 @@ function Gallery() {
   const videoItems = [
     {
       src: lake1Video,
+      thumbnail: lake1Thumbnail,
       title: 'Lake Hali',
       description: 'Explore the eerie Lake Hali, and follow in the steps of His Dynasty...',
     },
     {
       src: templeVideo,
+      thumbnail: templeThumbnail,
       title: 'Antarctic Temple',
       description: 'Discover what lies beneath the Antarctica, hidden for aeons...',
     },
     {
       src: village1Video,
+      thumbnail: village1Thumbnail,
       title: 'Wichport Village',
       description: 'Look for survivors in the fishing village of Wichport, if you dare...',
     },
     {
       src: forest2Video,
+      thumbnail: forest2Thumbnail,
       title: 'The Forest',
       description: 'Your sanity limits will be tested, for the King in Yellow beckons...',
     },
     {
       src: asylum1Video,
+      thumbnail: asylum1Thumbnail,
       title: 'Saint H.P. Lunatic Asylum',
       description: 'Navigate the halls of the, possibly, abandoned Asylum...',
     },
     {
       src: forest1Video,
+      thumbnail: forest1Thumbnail,
       title: 'The Forest',
       description: 'In the misty woods, the line between sanity and madness fades...',
     },
     {
       src: hyadesVideo,
+      thumbnail: hyadesThumbnail,
       title: 'Hyades\' Song',
       description: 'The haunting melody of the Hyades echoes, luring all to a forgotten fate...',
     },
     {
       src: lake2Video,
+      thumbnail: lake2Thumbnail,
       title: 'Lake Hali',
       description: 'Beneath the twin suns, the waters of Lake Hali conceal ancient secrets...',
     },
     {
       src: expeditionVideo,
+      thumbnail: expeditionThumbnail,
       title: 'Antarctic Expedition',
       description: 'In the frozen wastelands, secrets buried beneath ice threaten to awaken...',
     },
     {
       src: asylum2Video,
+      thumbnail: asylum2Thumbnail,
       title: 'Saint H.P. Lunatic Asylum',
       description: 'Sanity is a fleeting illusion, lost to the King\'s will...',
     },
     {
       src: village2Video,
+      thumbnail: village2Thumbnail,
       title: 'Wichport Village',
       description: 'The village hides its secrets, but the shadows reveal more than they should...',
     },
     {
       src: asylum3Video,
+      thumbnail: asylum3Thumbnail,
       title: 'Saint H.P. Lunatic Asylum',
       description: 'Within these walls, madness reigns and reality unravels...',
     },
     {
       src: asylum4Video,
+      thumbnail: asylum4Thumbnail,
       title: 'Saint H.P. Lunatic Asylum',
       description: 'The truth lies within, but only madness can find it...',
     },
     {
       src: bunkerVideo,
+      thumbnail: bunkerThumbnail,
       title: 'Bunker',
       description: 'Sanity is a fleeting illusion, lost to the King\'s will...',
     },
     {
       src: nightfallVideo,
+      thumbnail: nightfallThumbnail,
       title: 'Nightfall',
       description: 'Strange moons rise, and the shadows of Carcosa stretch endlessly...',
     },
     {
       src: village3Video,
+      thumbnail: village3Thumbnail,
       title: 'Wichport Village',
       description: 'Beneath its silence, Wichport hides ancient and unsettling truths...',
     },
@@ -300,33 +345,51 @@ function Gallery() {
     },
   ];
 
-  const renderMediaItem = (item, index, isVideo = false) => {
+  const renderVideoItem = (item, index) => {
     return (
-      <div className="carousel-item" key={`${isVideo ? 'video' : 'image'}-${index}`}>
-        {isVideo ? (
-          <LazyLoad height={200} offset={100} once>
+      <div className="carousel-item" key={`video-${index}`}>
+        <LazyLoad height={200} offset={100} once>
+          {loadedVideos.includes(index) ? (
             <video
+              ref={(el) => (videoRefs.current[index] = el)}
               className={`media-content ${isFullscreen ? 'fullscreen-video' : ''}`}
               controls
               preload="metadata"
-              onClick={(e) => handleVideoClick(e.target)}
             >
               <source src={item.src} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
-          </LazyLoad>
-        ) : (
-          <LazyLoad height={200} offset={100} once>
-            <div className="image-wrapper" onClick={() => handleImageClick(item.src)}>
+          ) : (
+            <div className="thumbnail-wrapper" onClick={() => handleVideoClick(index)}>
               <img
-                src={item.src}
+                src={item.thumbnail}
                 alt={item.title}
                 className="media-content"
-                onError={handleImageError}
               />
+              <div className="button-overlay play-button"></div>
             </div>
-          </LazyLoad>
-        )}
+          )}
+        </LazyLoad>
+        <div className="carousel-item-title">{item.title}</div>
+        <div className="carousel-item-description">{item.description}</div>
+      </div>
+    );
+  };
+
+  const renderImageItem = (item, index) => {
+    return (
+      <div className="carousel-item" key={`image-${index}`}>
+        <LazyLoad height={200} offset={100} once>
+          <div className="thumbnail-wrapper" onClick={() => handleImageClick(item.src)}>
+            <img
+              src={item.src}
+              alt={item.title}
+              className="media-content"
+              onError={handleImageError}
+            />
+            <div className="button-overlay open-button"></div>
+          </div>
+        </LazyLoad>
         <div className="carousel-item-title">{item.title}</div>
         <div className="carousel-item-description">{item.description}</div>
       </div>
@@ -340,14 +403,14 @@ function Gallery() {
       {/* Video Carousel */}
       <div className="gallery-container">
         <Slider {...settings}>
-          {videoItems.map((item, index) => renderMediaItem(item, index, true))}
+          {videoItems.map((item, index) => renderVideoItem(item, index))}
         </Slider>
       </div>
 
       {/* Image Carousel */}
       <div className="gallery-container">
         <Slider {...settings}>
-          {imageItems.map((item, index) => renderMediaItem(item, index, false))}
+          {imageItems.map((item, index) => renderImageItem(item, index))}
         </Slider>
       </div>
 
